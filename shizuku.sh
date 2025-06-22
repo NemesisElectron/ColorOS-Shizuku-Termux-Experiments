1c673d89dee1bb0ba2810b4db89cb3219f556a18
NemesisElectron <nemesis_electron@proton.me>
2025-06-22 18:29:59 +0300

    Update shizuku.sh

--- shizuku.sh
+++ shizuku.sh
@@ -1,3 +1,5 @@
+#!/system/bin/sh
+
 curl --output ../usr/bin/rish_shizuku.dex https://raw.githubusercontent.com/NemesisElectron/ColorOS-Shizuku-Termux-Experiments/refs/heads/main/rish_shizuku.dex
 curl --output ../usr/bin/rish https://raw.githubusercontent.com/NemesisElectron/ColorOS-Shizuku-Termux-Experiments/refs/heads/main/rish
 
